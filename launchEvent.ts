9f68f5d9cbed01319f91ff42bab84e8b0d0687ac
diegoxter <diegoxter1006@gmail.com>
2022-11-08 10:25:47 -0300

    Now it works

--- launchEvent.ts
+++ launchEvent.ts
@@ -4,13 +4,14 @@ const eventTesterABI = require("./abis/EventTester.json")
 require('dotenv').config()
 
 async function emitEvent() {
-    const [alice] = await ethers.getSigners()
-
     const eventTesterAddress = "0x6D3931ECd8f4E74FD4Be0790842DdD855a648109"
-    const provider = new ethers.providers.WebSocketProvider(
-       `https://${process.env.RIVET_KEY}.sepolia.ws.rivet.cloud`
+    const provider = new ethers.providers.JsonRpcProvider(
+       `https://${process.env.RIVET_KEY}.sepolia.rpc.rivet.cloud/`
     )
+    const alice = await ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+
     const contract = new ethers.Contract(eventTesterAddress, eventTesterABI, provider)
+    console.log('Connected to: ' + (await provider.getNetwork()).name)
 
     let rl = readline.createInterface({
     input: process.stdin,
@@ -25,12 +26,12 @@ async function emitEvent() {
             case 'y':
                 console.log("Lanzando evento")
 
-                console.log (await contract.connect(alice).emitTestEvent())
+                await contract.connect(alice.connect(provider)).emitTestEvent()
             
                 console.log("Evento lanzado")
                 break;
             case 'n':
-                console.log('Sorry! Closing');
+                console.log('Closing');
                 process.exit()
             default:
                 console.log('Say what? I might have heard `' + line.trim() + '`');
